460991310b8f16e13b36743a5a831829c0d4090a
Miguel Cordero Collar <mikelon797@gmail.com>
2020-10-02 22:32:32 +0200

    Landing Backbone

--- app.py
+++ app.py
@@ -18,25 +18,7 @@ db = SQLAlchemy(app)
 login=LoginManager(app)
 login.login_view = 'login'
 
-#--------------
-#Commands CLI
-#--------------
 
-def create_db():
-    db.create_all()
-
-    
-def drop_db():
-    #"Cleans database
-    db.drop_all()
-
-def commands_init_app(app):
-    print("creating commands")
-    # add multiple commands in a bulk
-    for command in [create_db, drop_db]:
-        app.cli.add_command(app.cli.command()(command))
-
-commands_init_app(app)
 
 #--------------
 #Login Manager
@@ -221,6 +203,7 @@ def get_timeline(expenses,incomes):
         data.append([item.date.strftime("%Y-%m-%d"),balance])
     
     return data
+
 #--------------
 #Pages
 #--------------
@@ -372,135 +355,15 @@ def login_post():
     return render_template("login.html")
 
 
-@app.route("/plot/")
-def plot():
-    import pandas_datareader.data as web
-    import datetime
-    from bokeh.plotting import figure, output_file, show
-    from bokeh.embed import components
-    from bokeh.resources import CDN
-
-    start = datetime.datetime(2019, 1, 1)
-    end = datetime.datetime(2020, 4, 8)
-    company = "TSLA"
-    df = web.DataReader(company, "stooq")
-
-    def inc_dec(c, o):
-        if c > o:
-            value = "Increase"
-        elif c < o:
-            value = "Decrease"
-        else:
-            value = "Equal"
-        return value
-
-    df["Status"] = [inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
-
-    df["Middle"] = (df.Open + df.Close) / 2
-    df["Height"] = abs(df.Open - df.Close)
-
-    p = figure(x_axis_type="datetime", width=1000, height=300)
-    p.title.text = "Candlestick Chart of " + company
-    p.grid.grid_line_alpha = 0.3
-    hours_12 = 12 * 60 * 60 * 1000
-
-    p.segment(df.index, df.High, df.index, df.Low, color="gray")
-
-    p.rect(
-        df.index[df.Status == "Increase"],
-        df.Middle[df.Status == "Increase"],
-        hours_12,
-        df.Height[df.Status == "Increase"],
-        fill_color="green",
-        line_color="green",
-    )
-    p.rect(
-        df.index[df.Status == "Decrease"],
-        df.Middle[df.Status == "Decrease"],
-        hours_12,
-        df.Height[df.Status == "Increase"],
-        fill_color="red",
-        line_color="red",
-    )
-
-    script1, div1 = components(p)
-    cdn_js = CDN.js_files[0]
-    cdn_css = CDN.css_files
+@app.route("/landing/")
+def landing():
+   
 
     return render_template(
-        "plot.html",
-        script1=script1,
-        div1=div1,
-        cdn_js=cdn_js,
-        cdn_css=cdn_css,
-        company=company,
+        "landing.html"
     )
 
 
-@app.route("/plot/", methods=["POST"])
-def plot_post():
-    import pandas_datareader.data as web
-    import datetime
-    from bokeh.plotting import figure, output_file, show
-    from bokeh.embed import components
-    from bokeh.resources import CDN
-
-    start = datetime.datetime(2019, 1, 1)
-    end = datetime.datetime(2020, 4, 8)
-
-    company = request.form["company"]
-    df = web.DataReader(company, "stooq")
-
-    def inc_dec(c, o):
-        if c > o:
-            value = "Increase"
-        elif c < o:
-            value = "Decrease"
-        else:
-            value = "Equal"
-        return value
-
-    df["Status"] = [inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
-
-    df["Middle"] = (df.Open + df.Close) / 2
-    df["Height"] = abs(df.Open - df.Close)
-
-    p = figure(x_axis_type="datetime", width=1000, height=300)
-    p.title.text = "Candlestick Chart of " + company
-    p.grid.grid_line_alpha = 0.3
-    hours_12 = 12 * 60 * 60 * 1000
-
-    p.segment(df.index, df.High, df.index, df.Low, color="gray")
-
-    p.rect(
-        df.index[df.Status == "Increase"],
-        df.Middle[df.Status == "Increase"],
-        hours_12,
-        df.Height[df.Status == "Increase"],
-        fill_color="green",
-        line_color="green",
-    )
-    p.rect(
-        df.index[df.Status == "Decrease"],
-        df.Middle[df.Status == "Decrease"],
-        hours_12,
-        df.Height[df.Status == "Increase"],
-        fill_color="red",
-        line_color="red",
-    )
-
-    script1, div1 = components(p)
-    cdn_js = CDN.js_files[0]
-    cdn_css = CDN.css_files
-
-    return render_template(
-        "plot.html",
-        script1=script1,
-        div1=div1,
-        cdn_js=cdn_js,
-        cdn_css=cdn_css,
-        company=company,
-    )
 
 
 @app.route("/about/")
