a6e3c657932ed255554ec275702b560e89cf357b
Miguel Cordero Collar <mikelon797@gmail.com>
2020-10-12 01:03:17 +0200

    Close modal updated

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -9,3 +9,14 @@ $('#but-cat').click(function () {
     $('.container-modal').fadeOut( "slow", function() {  });
  
   });
+
+  $(document).mouseup(function(e) 
+{
+    var container = $("#cat-modal");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+      $('.container-modal').fadeOut( "slow", function() {  });
+    }
+});
