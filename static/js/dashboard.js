5342d0b8002ad1af262dcaa2e5a421fab17f027e
Miguel Cordero Collar <mikelon797@gmail.com>
2020-10-11 23:28:02 +0200

    Add category improven

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,2 +1,11 @@
 
 
+$('#but-cat').click(function () {
+   $('.container-modal').fadeIn( "slow", function() {  });
+
+ });
+
+ $('#close-cat').click(function () {
+    $('.container-modal').fadeOut( "slow", function() {  });
+ 
+  });
