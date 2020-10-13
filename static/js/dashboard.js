a16be21b8dcbddb52b6518020445ac64c9ce76a3
Miguel Cordero Collar <mikelon797@gmail.com>
2020-10-13 19:47:43 +0200

    Color font landing fixed

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,22 +1,19 @@
-
-
 $('#but-cat').click(function () {
-   $('.container-modal').fadeIn( "slow", function() {  });
+  $('.container-modal').fadeIn("slow", function () {});
 
- });
+});
 
- $('#close-cat').click(function () {
-    $('.container-modal').fadeOut( "slow", function() {  });
- 
-  });
+$('#close-cat').click(function () {
+  $('.container-modal').fadeOut("slow", function () {});
 
-  $(document).mouseup(function(e) 
-{
-    var container = $("#cat-modal");
+});
 
-    // if the target of the click isn't the container nor a descendant of the container
-    if (!container.is(e.target) && container.has(e.target).length === 0) 
-    {
-      $('.container-modal').fadeOut( "slow", function() {  });
-    }
+
+$(document).mouseup(function (e) {
+  var container = $("#cat-modal");
+
+  // if the target of the click isn't the container nor a descendant of the container
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    $('.container-modal').fadeOut("slow", function () {});
+  }
 });
